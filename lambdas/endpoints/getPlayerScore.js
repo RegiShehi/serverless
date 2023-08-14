583b6de7d005dd77464b3ffd81d93b0db1ff2c96
Shehi <rshehi@deloittece.com>
2023-08-14 11:54:50 +0200

    Add Lambda Hooks library

--- lambdas/endpoints/getPlayerScore.js
+++ lambdas/endpoints/getPlayerScore.js
@@ -1,23 +1,17 @@
 const Responses = require('../common/API_Responses');
 const Dynamo = require('../common/Dynamo');
+const { withHooks } = require('../common/hooks');
 
 const tableName = process.env.tableName;
 
-exports.handler = async (event) => {
-  console.log('event', event);
-
-  if (!event.pathParameters || !event.pathParameters.ID) {
-    // failed without an ID
+const handler = async (event) => {
+  if (!event.pathParameters.ID) {
     return Responses._400({ message: 'Missing ID from the path' });
   }
 
   let ID = event.pathParameters.ID;
 
-  const user = await Dynamo.get(ID, tableName).catch((err) => {
-    console.log('Error is Dynamo Get', err);
-
-    return null;
-  });
+  const user = await Dynamo.get(ID, tableName);
 
   if (!user) {
     return Responses._400({ message: 'Failed to get user by ID' });
@@ -25,3 +19,5 @@ exports.handler = async (event) => {
 
   return Responses._200({ user });
 };
+
+exports.handler = withHooks(handler);
